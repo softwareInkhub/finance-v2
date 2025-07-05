d8e174fbefecf54e311ff779f898f54209610817
softwareInkhub <software.inkhub@gmail.com>
2025-07-05 12:47:04 +0530

    updates by monty dashbopard route fixed

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -34,7 +34,7 @@ export default function Sidebar({ onItemClick }: SidebarProps) {
   }, []);
 
   const menuItems = [
-    { name: 'Dashboard', path: '/dashboard', icon: RiDashboardLine },
+    { name: 'Dashboard', path: '/', icon: RiDashboardLine },
     { name: 'Banks', path: '/banks', icon: RiBankLine },
     { name: 'Tags', path: '/tags', icon: RiPriceTag3Line },
     { name: 'File Converter', path: '/convert-file', icon: RiPriceTag3Line },
